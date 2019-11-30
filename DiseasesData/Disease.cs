4cab0cf98b1cac610c763652928979714837e65a
Cezar Petriuc <cezar.c.petriuc@gmail.com>
2019-11-30 19:18:07 +0200

    code-first ORM

---
+++ DiseasesData/Disease.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using System.Linq;
+
+namespace DiseasesData
+{
+    public class Disease
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; private set; }
+
+        public virtual ICollection<Name> Names { get; private set; }
+
+        public ICollection<string> StringNames
+        {
+            get => Names.Select(n => n.Value).ToList();
+        }
+
+        public Disease()
+        {
+            this.Id = Guid.NewGuid();
+        }
+    }
+}
---
+++ DiseasesData/DiseasesContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace DiseasesData
+{
+    public class DiseasesContext : DbContext
+    {
+        public DbSet<Disease> Diseases { get; set; }
+
+        public DbSet<Name> Names { get; set; }
+
+        public DbSet<Cause> Causes { get; set; }
+
+        public DiseasesContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=.;Database=DiseasesDb;Trusted_Connection=True;MultipleActiveResultSets=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cause>()
+                .HasOne(typeof(Disease), "Disease")
+                .WithMany()
+                .HasForeignKey("DiseaseId")
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Cause>()
+                .HasOne(typeof(Disease), "CausedBy")
+                .WithMany()
+                .HasForeignKey("CausedById")
+                .OnDelete(DeleteBehavior.Restrict);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
