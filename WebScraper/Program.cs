237675114873e46a99c20a86d960e0aa27f3a97e
Cezar Petriuc <cezar.c.petriuc@gmail.com>
2019-12-08 17:41:07 +0200

    Added web scraper
    Doesn't work tho...

---
+++ WebScraper/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+
+namespace WebScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///*
+            string url = "http://diseasesdatabase.com";
+            
+            using (WebClient client = new WebClient())
+            {
+                string html = client.DownloadString(url);
+                Console.WriteLine(html);
+            }
+            //*/
+            /*
+            WebRequest req = HttpWebRequest.Create(url);
+            req.Method = "GET";
+
+            string source;
+            using (StreamReader reader = new StreamReader(req.GetResponse().GetResponseStream()))
+            {
+                source = reader.ReadToEnd();
+            }
+
+            Console.WriteLine(source);
+            */
+        }
+    }
+}
