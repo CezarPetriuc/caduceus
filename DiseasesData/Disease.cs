cb8ac99f5c64c53eb50362aa8b581efcec65b1ed
Cezar Petriuc <cezar.c.petriuc@gmail.com>
2019-12-24 13:10:52 +0200

    added easy name access property

--- DiseasesData/Disease.cs
+++ DiseasesData/Disease.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Text;
 using System.Linq;
 
 namespace DiseasesData
@@ -13,11 +12,16 @@ namespace DiseasesData
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public Guid Id { get; private set; }
 
+        public string StringName
+        {
+            get => this.StringNames.First();
+        }
+        
         public virtual ICollection<Name> Names { get; private set; }
 
         public ICollection<string> StringNames
         {
-            get => Names?.Select(n => n.Value).ToList();
+            get => Names?.Select(n => n.Value.Trim()).ToList();
         }
 
         public Disease()
