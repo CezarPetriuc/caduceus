3c03250afe32f108b378ec08f1f5c393d9f41c6b
catalinTM <35262902+catalinTM@users.noreply.github.com>
2020-01-02 12:29:31 +0200

    Add files via upload

---
+++ Caduceus/Data/CaduceusContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication2.Models;
+
+namespace WebApplication2.Data
+{
+    public class CaduceusContext : DbContext
+    {
+        public CaduceusContext(DbContextOptions<CaduceusContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Simptom> Simptom { get; set; }
+    }
+}
+    
