0877d2b8911a5333dd962e1fa3f2c3c52495c107
Cezar Petriuc <cezar.c.petriuc@gmail.com>
2020-01-10 14:48:25 +0200

    added data conversion method for easy use in ML

--- DiseasesData/DiseasesContext.cs
+++ DiseasesData/DiseasesContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace DiseasesData
 {
@@ -16,6 +17,33 @@ namespace DiseasesData
             Database.EnsureCreated();
         }
 
+        public string GetDiseaseName(Disease disease)
+        {
+            return this.Names.Where(n => n.DiseaseId == disease.Id).FirstOrDefault().Value;
+        }
+
+        public List<List<string>> GetMlData()
+        {
+            List<List<string>> result = new List<List<string>>();
+            int i = 0;
+            foreach (Disease disease in this.Diseases)
+            {
+                List<string> aux = new List<string>();
+
+                var comorbids = this.Causes
+                    .Where(c => c.Disease == disease)
+                    .Select(c => c.CausedBy)
+                    .ToList();
+
+                foreach(Disease comorbid in comorbids)
+                    aux.Add(this.GetDiseaseName(comorbid));
+                aux.Add(this.GetDiseaseName(disease));
+
+                result.Add(aux);
+            }
+            return result;
+        }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
@@ -24,6 +52,13 @@ namespace DiseasesData
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Disease>()
+                .HasMany(m => m.Names)
+                .WithOne(m => m.Disease)
+                .HasForeignKey(m => m.DiseaseId);
+            
             modelBuilder.Entity<Cause>()
                 .HasOne(typeof(Disease), "Disease")
                 .WithMany()
@@ -38,9 +73,7 @@ namespace DiseasesData
 
             modelBuilder.Entity<Cause>()
                 .HasIndex(p => new { p.DiseaseId, p.CausedById })
-                .IsUnique();
-            
-            base.OnModelCreating(modelBuilder);
+                .IsUnique();    
         }
     }
 }
